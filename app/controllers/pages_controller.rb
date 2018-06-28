af39c6e92f5f82a28adbc83cfff213493bf13a30
Aaron James George <ajgeorge@hotmail.co.uk>
2018-06-28 13:26:12 +0000

    Added about view(page), route and controller action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
-    def home
+    def home #corresponding "home.html.erb" file can be found in the saasapp/views/pages directory
+    end
+    
+    def about #corresponding "about.html.erb" file can be found in the saasapp/views/pages directory
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-    root to: 'pages#home'
+    root to: 'pages#home' #route for the home page
+    get 'about', to: 'pages#about' #get the about page (could use / in front of "about" but don't need it) and routes it to pages#about
 end
