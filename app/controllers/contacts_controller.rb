7928741a6ac806d8314c7de8b4006cd651cb7e73
Aaron James George <ajgeorge@hotmail.co.uk>
2018-07-22 12:51:01 +0000

    Added create action to save contact form inputs to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,29 @@
 class ContactsController < ApplicationController
     def new #views file has to correspond with this name
+        @contact = Contact.new #everytime somebody pulls up the contacts form, rails will generate a new contact object
+        #the @contact is an "instance variable"
     end
+    
+    def create #method to save user input on the contact form
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            redirect_to new_contact_path, notice: "Message sent."
+        else
+            redirect_to new_contact_path, notice: "Error occurred."
+        end
+    end
+    
+    private #security feature to ensure correct attributes are entered I guess?
+        def contact_params
+            params.required(:contact).permit(:name, :email, :comments)
+        end
 end
+
+=begin 
+The "def create" saves a new contact object. The if statement is run to check if
+the contact message was saved, and if so redirect the user to the contact page
+with a message stating that the message was sent successfully.
+
+The else states that if the message failed to send, then send the user to the
+contact form page and notify the user that the message failed to send.
+=end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ActiveRecord::Base
-     
+    #attributes for contact already specified in the db file
+    #check schema file
 end
