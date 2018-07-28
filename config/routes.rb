81bbe255a3793afc750ff066b965f6cbc0690e94
Aaron James George <ajgeorge@hotmail.co.uk>
2018-07-28 16:15:50 +0000

    Cleared up routes file, changed contact url to contact-us

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     root to: 'pages#home' #route for the home page
     get 'about', to: 'pages#about' #when a customer makes a GET request to /about, we want the rails app to look inside the pages controller and run its "about" action. #could also use / in fromt of about, but it is not necessary
-    resources :contacts
-    get 'contact-us', to: 'contacts#new' #set new contacts controller URL to "contact-us"
+    resources :contacts, only: :create
+    get 'contact-us', to: 'contacts#new', as: 'new_contact' #set new contacts controller URL to "contact-us"
 end
