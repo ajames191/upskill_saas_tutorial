b3eea4575a5dab1a7f82ed5c2e844ba62e541cd7
Aaron James George <ajgeorge@hotmail.co.uk>
2018-07-28 15:40:59 +0000

    Added custom url to routes file contact-us

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
     root to: 'pages#home' #route for the home page
     get 'about', to: 'pages#about' #when a customer makes a GET request to /about, we want the rails app to look inside the pages controller and run its "about" action. #could also use / in fromt of about, but it is not necessary
     resources :contacts
+    get 'contact-us', to: 'contacts#new' #set new contacts controller URL to "contact-us"
 end
