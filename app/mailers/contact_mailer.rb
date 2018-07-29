63181bba34cdf4c105cfc0c20b3610b969f986ec
Aaron James George <ajgeorge@hotmail.co.uk>
2018-07-29 15:24:21 +0000

    Added new email address to mailer file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'devmatch@hotmail.com'
+    default to: 'devmatchblog@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
