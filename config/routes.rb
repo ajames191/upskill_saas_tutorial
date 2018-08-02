90acd3f99ce65c2bb97ea969447437f715668662
Aaron James George <ajgeorge@hotmail.co.uk>
2018-08-02 15:19:03 +0000

    Installed Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
     root to: 'pages#home' #route for the home page
     get 'about', to: 'pages#about' #when a customer makes a GET request to /about, we want the rails app to look inside the pages controller and run its "about" action. #could also use / in fromt of about, but it is not necessary
     resources :contacts, only: :create
