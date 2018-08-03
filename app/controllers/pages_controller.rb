5949b59376e5878248877dffafc11ecbc0816250
Aaron James George <ajgeorge@hotmail.co.uk>
2018-08-03 12:10:45 +0000

    Added plan variables and parameters to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
+    # GET request for / which is our home page
     def home #corresponding "home.html.erb" file can be found in the saasapp/views/pages directory
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
     end
     
+    # GET request for about path which is our about page
     def about #corresponding "about.html.erb" file can be found in the saasapp/views/pages directory
     end
 end
