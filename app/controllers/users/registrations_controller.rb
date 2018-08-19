924bd946eec511aed0230b300445d689257b1224
Aaron James George <ajgeorge@hotmail.co.uk>
2018-08-19 14:45:47 +0000

    Corrected registrations_controller code to fix error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-    before_action :select_plan, only: new
+    before_action :select_plan, only: :new
     # Extend default Devise gem behaviour so that
     # users signing up with the Pro account (plan ID 2)
     # save with a special Stripe subscription function
